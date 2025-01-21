63d623f62bfc2e36003e8b6d47c4227143d3418f
Karoline <karoline.rezende@agendaassessoria.com.br>
2025-01-21 13:21:42 -0400

    first commit

---
+++ components/Button.vue
@@ -0,0 +1,13 @@
+<template >
+    <button :class="class" :disabled="disabled" @click="handleClick">
+        <slot name="icon"/>
+        <slot/>
+    </button>
+</template>
+<script>
+    const props = defineProps
+
+</script>
+<style>
+    
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx,vue}',
+  ],
+  theme: {
+    colors: {
+      primary: '#7ED4D9',
+      'primary-dark': '#4FA5AA',
+      secondary: '#FFFFFF',
+      tertiary: '#4FA5AA',
+      quaternary: '#267B80',
+      quinary: '#0B5155',
+      danger: '#BB2D14',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
