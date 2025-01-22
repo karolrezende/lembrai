2cdfce4d35d750854f680f3eafdf9b93662f311c
karol <karolrds01@gmail.com>
2025-01-21 23:47:27 -0400

    adding configs

--- components/Button.vue
+++ components/Button.vue
@@ -1,13 +1,43 @@
 <template >
-    <button :class="class" :disabled="disabled" @click="handleClick">
+    <button :class="defaultClass" :disabled="disabled" @click="handleClick">
         <slot name="icon"/>
         <slot/>
     </button>
 </template>
 <script>
-    const props = defineProps
+    const props = defineProps({
+        variant: {
+            string, 
+            default: 'primary',
+            validator(value) { return ['primary', 'secondary', 'danger',].includes(value)}
+        },
+        size: {
+            string,
+            default: 'sm',
+            validator(value) { return ['sm', 'md', 'lg',].includes(value)}
+        },
+        disabled: {
+            boolean,
+            default: false
+        }
+    })
+
+    const defaultClass = computed(() => [
+        'btn',
+        `btn-${props.variant}`,
+        `btn-${props.size}`,
+    ])
+
+    
 
 </script>
 <style>
-    
+    .btn-primary {
+        @apply bg-primary text-secondary 
+    }
+
+    .btn-secondary {
+        @apply bg-secondary text-primary 
+    }
+
 </style>
